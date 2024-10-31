d603184928f041e48af878cd10038622b634e971
kgaohahaha <15296612347@163.com>
2024-10-31 17:49:51 +0800

    feat: modify todoitem with time limit

--- TodoItems.Core/TodoItem.cs
+++ TodoItems.Core/TodoItem.cs
@@ -1,9 +1,45 @@
-﻿namespace TodoItems.Core;
+﻿using System.Runtime.CompilerServices;
+using TodoItems.Core.services;
+
+namespace TodoItems.Core;
 
 public class TodoItem
 {
+    public string? Id { get; set; }
+    public string Description { get; set; }
+    public DateTime? CreateTime { get; set; }
+    public DateTime[]? ModifyTime { get; set; }
+    public Boolean IsComplete { get; set; }
+    public Boolean IsFavorite { get; set; }
+
     public string GetId()
     {
-        return "1";
+        return this.Id;
+    }
+
+    public TodoItem ModifyTodoItem(TodoItem oldItem,TodoItem newItem)
+    {
+        if (oldItem.ModifyTime[oldItem.ModifyTime.Length-1].Date!=DateTime.Now.Date)
+        {
+            oldItem.ModifyTime = [DateTime.Now];
+            ModifyItemDescription(oldItem, newItem);
+        }
+        else
+        {
+            if (oldItem.ModifyTime.Length >= 3)
+            {
+                throw new Exception("No modify time");
+            }
+            else
+            {
+                ModifyItemDescription(oldItem, newItem);
+            }
+        }
+        return oldItem;
+    }
+
+    private static void ModifyItemDescription(TodoItem oldItem, TodoItem newItem)
+    {
+        oldItem.Description = newItem.Description;
     }
 }
--- TodoItems.Test/TodoItemTest.cs
+++ TodoItems.Test/TodoItemTest.cs
@@ -5,9 +5,87 @@ namespace TodoItems.Test;
 public class TodoItemTest
 {
     [Fact]
-    public void should_return_2_when_add_1_1()
+    public void should_update_when_time_less_than_three()
     {
-        var todoItem = new TodoItem();
-        Assert.Equal("1", todoItem.GetId());
+        var oldItem = new TodoItem()
+        {
+            Id = "1",
+            Description = "old item description",
+            CreateTime = DateTime.Now,
+            IsComplete = false,
+            IsFavorite = false,
+            ModifyTime= [],
+        };
+        var updateItem = new TodoItem()
+        {
+            Id = "1",
+            Description = "should update description",
+            CreateTime = DateTime.Now,
+            IsComplete = false,
+            IsFavorite = false,
+            ModifyTime = [],
+        };
+        try
+        {
+            var updatedItem = oldItem.ModifyTodoItem(oldItem, updateItem);
+        }
+        catch (Exception ex) { }
+        Assert.Equal("should update description",updateItem.Description);
+    }
+
+    [Fact]
+    public void should_throw_exception_when_time_more_than_three()
+    {
+        var oldItem = new TodoItem()
+        {
+            Id = "1",
+            Description = "old item description",
+            CreateTime = DateTime.Now,
+            IsComplete = false,
+            IsFavorite = false,
+            ModifyTime = [new DateTime(2024, 10, 31, 00, 00, 01), new DateTime(2024, 10, 31, 00, 01, 01), new DateTime(2024, 10, 31, 01, 00, 01)],
+        };
+        var updateItem = new TodoItem()
+        {
+            Id = "1",
+            Description = "should update description",
+            CreateTime = DateTime.Now,
+            IsComplete = false,
+            IsFavorite = false,
+        };
+        try
+        {
+            oldItem.ModifyTodoItem(oldItem, updateItem);
+        }
+        catch (Exception ex) {
+            Assert.Equal(ex.Message, "No modify time");
+        }
+        Assert.Equal("old item description",oldItem.Description);
+    }
+
+    [Fact]
+    public void should_update_todoItem_when_across_day()
+    {
+        var oldItem = new TodoItem()
+        {
+            Id = "1",
+            Description = "old item description",
+            CreateTime = DateTime.Now,
+            IsComplete = false,
+            IsFavorite = false,
+            ModifyTime = [new DateTime(2024, 10, 30, 00, 00, 01), new DateTime(2024, 10, 30, 00, 01, 01), new DateTime(2024, 10, 30, 01, 00, 01)],
+        };
+        var updateItem = new TodoItem()
+        {
+            Id = "1",
+            Description = "should update description",
+            CreateTime = DateTime.Now,
+            IsComplete = false,
+            IsFavorite = false,
+        };
+        var updatedItem = oldItem.ModifyTodoItem(oldItem, updateItem);
+
+        Assert.Equal("should update description", updatedItem.Description);
+        Assert.Equal(1, updatedItem.ModifyTime.Length);
     }
 }
