1a0c6775352035648a75d556c627b6cc03cee345
WZD09 <2101210655@stu.pku.edu.cn>
2024-10-31 19:44:50 +0800

    feat: impl GetItemsByDueDate

---
+++ TodoItems.Core/Service/TodosRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoItems.Core;
+
+namespace TodoItems.Core.Service
+{
+    public class TodosRepository
+    {
+        static public List<TodoItem> itemsCollection = new List<TodoItem>();
+        public List<TodoItem> GetItemsByDueDate(DateTimeOffset dueDate)
+        {
+            var result =  itemsCollection.Where(item =>
+            {
+                return item.DueDate == dueDate;
+            }).ToList();
+            return result;
+        }
+    }
+}
