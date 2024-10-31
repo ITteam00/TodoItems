0a3e7c205cae3dc0cddd9988d6c4cb72420896b1
Xi Wu <XWu35@slb.com>
2024-10-31 15:39:27 +0800

    feat: add property about item

--- TodoItems.Core/TodoItem.cs
+++ TodoItems.Core/TodoItem.cs
@@ -2,8 +2,18 @@
 
 public class TodoItem
 {
+    public string Id { get; set; }
+    public string Description { get; set; }
+    public bool IsDone { get; set; }
+    public bool IsFavorite { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.Now;
     public string GetId()
     {
         return "1";
+    }
+
+    public bool ModifyItem() 
+    {
+        return true;
     }
 }
