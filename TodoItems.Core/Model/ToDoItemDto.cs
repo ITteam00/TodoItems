9fd778358b8691a8b9d5e97e58dd3a83883d4644
yyu26 <yyu26@slb.com>
2024-10-31 15:38:10 +0800

    add TodoItem data model

---
+++ TodoItems.Core/Model/ToDoItemDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoItems.Core.Model
+{
+    public class ToDoItemDto
+    {
+        public required string Id { get; init; }
+        public string Description { get; set; } = string.Empty;
+        public bool isDone { get; set; } = false;
+        public DateTimeOffset CreatedTime { get; set; } = DateTimeOffset.Now;
+        public bool isFavorite { get; set; } = false;
+        public bool isDelete { get; set; } = false;
+        public List<DateTimeOffset> ModificationDateTimes { get; set; }
+    }
+}
