4c83b620c169099cb8e1107568ff7931cb033bce
yyu26 <yyu26@slb.com>
2024-10-31 15:50:39 +0800

    feat: add ModificationCount func and Test

--- TodoItems.Core/TodoItem.cs
+++ TodoItems.Core/TodoItem.cs
@@ -1,9 +1,17 @@
-﻿namespace TodoItems.Core;
+﻿using System;
+
+namespace TodoItems.Core;
 
 public class TodoItem
 {
     public string GetId()
     {
         return "1";
+    }
+
+    public int ModificationCount(List<DateTimeOffset> modificationDateTimes)
+    {
+        var count = modificationDateTimes.Count(d => d.Date == DateTimeOffset.UtcNow.Date); 
+        return count;
     }
 }
--- TodoItems.Test/TodoItemTest.cs
+++ TodoItems.Test/TodoItemTest.cs
@@ -1,3 +1,4 @@
+using System.Xml.Serialization;
 using TodoItems.Core;
 
 namespace TodoItems.Test;
@@ -9,5 +10,19 @@ public class TodoItemTest
     {
         var todoItem = new TodoItem();
         Assert.Equal("1", todoItem.GetId());
+    }
+
+    [Fact]
+    public void Should_return_modificationCount_when_call_()
+    {
+        List<DateTimeOffset> dateTimes = new List<DateTimeOffset>
+        {
+            new DateTimeOffset(2024, 10, 30, 14, 0, 0, TimeSpan.Zero),
+            new DateTimeOffset(2024, 10, 31, 9, 0, 0, TimeSpan.Zero),
+            new DateTimeOffset(2024, 10, 29, 18, 0, 0, TimeSpan.Zero),
+            new DateTimeOffset(2024, 10, 31, 17, 0, 0, TimeSpan.Zero)
+        };
+        var todoItem = new TodoItem();
+        Assert.Equal(2, todoItem.ModificationCount(dateTimes));
     }
 }
