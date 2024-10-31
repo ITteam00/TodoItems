f45a132249349a44092c609d925f8bd84a3e75d5
WZD09 <2101210655@stu.pku.edu.cn>
2024-11-01 00:07:45 +0800

    refractor: update service

---
+++ TodoItems.Core/TodoItemService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoItems.Core.Service;
+
+namespace TodoItems.Core
+{
+    public class TodoItemService
+    {
+        public TodoItem Create(string id, string description)
+        {
+            ITodosRepository todosRepository = new TodosRepository();
+            var user = new User(todosRepository);
+            var item = new TodoItem
+            {
+                Id = id,
+                Description = description,
+            };
+            user.AddOneItem(item);
+            return item;
+        }
+    }
+}
