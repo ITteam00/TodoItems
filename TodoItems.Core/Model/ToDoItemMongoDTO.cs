b3cec6055d3326849e83b65b731587ff8142fb43
yyu26 <yyu26@slb.com>
2024-10-31 19:00:13 +0800

    feat: add ToDoItemMongoDTO Class

---
+++ TodoItems.Core/Model/ToDoItemMongoDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoItems.Core.Model
+{
+    public class ToDoItemMongoDTO
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Description { get; set; } = string.Empty;
+        public bool isDone { get; set; }
+        public bool isFavorite { get; set; }
+
+        public DateTimeOffset CreatedTime { get; set; }
+
+        public List<DateTimeOffset> ModificationDateTimes { get; set; }
+    }
+}
