8293ec4d15345799a36a9e16f00f60c1474c1890
yyu26 <yyu26@slb.com>
2024-10-31 19:01:59 +0800

    feat: update service with test

---
+++ TodoItems.Core/Services/ToDoItemsService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using TodoItems.Core.Model;
+
+namespace TodoItems.Core.Services
+{
+    public class ToDoItemsService : IToDoItemsService
+    {
+        private readonly IMongoCollection<ToDoItemMongoDTO> _ToDoItemsCollection;
+
+        public int ModificationCount(List<DateTimeOffset> modificationDateTimes)
+        {
+            var count = modificationDateTimes.Count(d => d.Date == DateTimeOffset.UtcNow.Date);
+            return count;
+        }
+
+        public async Task CheckCountUpdateAsync(string id, ToDoItemDto updatedToDoItem)
+        {
+            var modificationCount = ModificationCount(updatedToDoItem.ModificationDateTimes);
+            if (modificationCount >= 3)
+            {
+                throw new TooManyEntriesException("to many");
+            }
+
+            await UpdateAsync(id, updatedToDoItem);
+        }
+
+
+        public async Task UpdateAsync(string id, ToDoItemDto updatedToDoItem)
+        {
+            
+        }
+    }
+
+    public class TooManyEntriesException : Exception
+    {
+        public TooManyEntriesException(string message) : base(message)
+        {
+        }
+    }
+}
--- TodoItems.Test/TodoItemTest.cs
+++ TodoItems.Test/TodoItemTest.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Xml.Serialization;
 using TodoItems.Core;
+using TodoItems.Core.Model;
+using TodoItems.Core.Services;
 
 namespace TodoItems.Test;
 
@@ -13,16 +16,56 @@ public class TodoItemTest
     }
 
     [Fact]
-    public void Should_return_modificationCount_when_call_()
+    public void Should_return_modificationCount_when_call_ModificationCount()
     {
         List<DateTimeOffset> dateTimes = new List<DateTimeOffset>
         {
             new DateTimeOffset(2024, 10, 30, 14, 0, 0, TimeSpan.Zero),
             new DateTimeOffset(2024, 10, 31, 9, 0, 0, TimeSpan.Zero),
             new DateTimeOffset(2024, 10, 29, 18, 0, 0, TimeSpan.Zero),
-            new DateTimeOffset(2024, 10, 31, 17, 0, 0, TimeSpan.Zero)
+            new DateTimeOffset(2024, 10, 31, 17, 0, 0, TimeSpan.Zero),
+            new DateTimeOffset(2024, 10, 31, 16, 2, 0, TimeSpan.Zero),
+            new DateTimeOffset(2024, 10, 31, 16, 2, 0, TimeSpan.Zero)
         };
-        var todoItem = new TodoItem();
-        Assert.Equal(2, todoItem.ModificationCount(dateTimes));
+        var todoItem = new ToDoItemsService();
+        Assert.Equal(4, todoItem.ModificationCount(dateTimes));
+    }
+
+    [Fact]
+    public async Task Should_throw_Exception_when_call_Update()
+    {
+        List<DateTimeOffset> dateTimes = new List<DateTimeOffset>
+        {
+            new DateTimeOffset(2024, 10, 30, 14, 0, 0, TimeSpan.Zero),
+            new DateTimeOffset(2024, 10, 31, 9, 0, 0, TimeSpan.Zero),
+            new DateTimeOffset(2024, 10, 29, 18, 0, 0, TimeSpan.Zero),
+            new DateTimeOffset(2024, 10, 31, 17, 0, 0, TimeSpan.Zero),
+            new DateTimeOffset(2024, 10, 31, 16, 2, 0, TimeSpan.Zero),
+            new DateTimeOffset(2024, 10, 31, 16, 2, 0, TimeSpan.Zero)
+        };
+        var updatedToDoItem = new ToDoItemDto
+        {
+            Description = "Test Description",
+            isDone = false,
+            isFavorite = false,
+            CreatedTime = DateTime.UtcNow,
+            ModificationDateTimes = dateTimes,
+            Id = "1",
+        };
+
+        var todoItem = new ToDoItemsService();
+        var str = "";
+        try
+        {
+            await todoItem.CheckCountUpdateAsync(updatedToDoItem.Id, updatedToDoItem);
+        }
+        catch (TooManyEntriesException e)
+        {
+            Console.Write(e);
+            str = e.Message;
+        }
+        
+
+        Assert.Equal(str, "to many");
     }
 }
