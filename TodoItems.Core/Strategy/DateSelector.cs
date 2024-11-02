7d7b0b42e22221ab56a3ab7f09aef8f1c10aa9cb
yyu26 <yyu26@slb.com>
2024-11-02 15:32:04 +0800

    feat: add strategy context `DateSelector`

---
+++ TodoItems.Core/Strategy/DateSelector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoItems.Core.Strategy
+{
+    public class DateSelector
+    {
+        private readonly IDateSelectionStrategy _strategy;
+
+        public DateSelector(string strategyType)
+        {
+            _strategy = GetStrategy(strategyType);
+        }
+
+        public DateTimeOffset SelectDate(Dictionary<DateTimeOffset, int> dateCounts)
+        {
+            return _strategy.SelectDate(dateCounts);
+        }
+
+        private IDateSelectionStrategy GetStrategy(string strategyType)
+        {
+            return strategyType switch
+            {
+                "closest" => new ClosestDateStrategy(),
+                "freest" => new FreestDateStrategy(),
+                _ => throw new ArgumentException("Invalid strategy type")
+            };
+        }
+    }
+}
