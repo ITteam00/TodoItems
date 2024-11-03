8c975a76bec7a3914f47ec1371dc903c97929b42
Xi Wu <XWu35@slb.com>
2024-11-03 14:40:05 +0800

    feat : implement `LeastCountDuedateStrategy` based on `IDuedateStrategy`

---
+++ TodoItems.Core/LeastCountDuedateStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoItems.Core
+{
+    public class LeastCountDuedateStrategy : IDuedateStrategy
+    {
+        public Task<DateTime?> SetDuedate(TodoItemDto todoItemDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
