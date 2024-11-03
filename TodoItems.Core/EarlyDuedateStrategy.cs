00b5d7fbb3d7f175193eb4d7f8bea810a7aa35a0
Xi Wu <XWu35@slb.com>
2024-11-03 14:41:42 +0800

    feat: implement `EarlyDuedateStrategy` based on `IDuedateStrategy`

---
+++ TodoItems.Core/EarlyDuedateStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoItems.Core
+{
+    public class EarlyDuedateStrategy : IDuedateStrategy
+    {
+        public Task<DateTime?> SetDuedate(TodoItemDto todoItemDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
